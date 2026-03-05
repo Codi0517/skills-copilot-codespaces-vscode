17d21432b22c1537d3802e483c6161aeb162335e
Codi0517 <inf24082@lehre.dhbw-stuttgart.de>
2026-03-05 09:58:14 +0000

    fix

--- comments.js
+++ comments.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-// create web server
+// Create web server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
